1887844db3584da4b2fe2408c0b5709190a68fa0
uifehnah <hanfeihu126@gmail.com>
2021-12-18 17:12:30 -0500

    test 1

--- jsWriteToText.js
+++ jsWriteToText.js
@@ -1,7 +1,9 @@
-console.log("YES")
-const fs = require('fs')
+function WriteToFile(passForm) {
 
-let data = "Learning how to write in a file."
-fs.writeFile('data.txt', data, (err) => {
-    if (err) throw err;
-})
+    set fso = CreateObject("Scripting.FileSystemObject");  
+    set s = fso.CreateTextFile("C:\test.txt", True);
+    s.writeline(document.passForm.input1.value);
+    s.writeline(document.passForm.input2.value);
+    s.writeline(document.passForm.input3.value);
+    s.Close();
+ }
