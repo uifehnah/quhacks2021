8c6c952078237c669280cf1e8430299e5f28605c
uifehnah <hanfeihu126@gmail.com>
2021-12-18 17:32:15 -0500

    data written to localhost

--- jsWriteToText.js
+++
@@ -1,9 +0,0 @@
-function WriteToFile(passForm) {
-
-    set fso = CreateObject("Scripting.FileSystemObject");  
-    set s = fso.CreateTextFile("C:\test.txt", True);
-    s.writeline(document.passForm.input1.value);
-    s.writeline(document.passForm.input2.value);
-    s.writeline(document.passForm.input3.value);
-    s.Close();
- }
